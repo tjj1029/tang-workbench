33ea6113fce835c9d45281cf38ba40a0ed22a6e6
TangTang <tang@workbench.app>
2026-07-27 01:13:43 +0800

    v5: 英语App跳转+统一打卡提醒+未完成通知

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='tangworkbench-v4';
+const CACHE='tangworkbench-v5';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
